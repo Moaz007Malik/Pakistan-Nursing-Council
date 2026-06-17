455eaaab5a15419a03e4c069d298408ed4686331
Moaz Malik <moaz007malik@gmail.com>
2026-06-17 19:45:50 +0500

    dfgh

--- backend/api/health.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../../.env') });
-
-const { applyCorsHeaders, sendJson } = require('./cors');
-
-module.exports = async (req, res) => {
-  applyCorsHeaders(req, res);
-  sendJson(res, 200, { status: 'ok', timestamp: new Date().toISOString() });
-};
